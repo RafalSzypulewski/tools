7b2bda23f846a5a9f8656d2fc5cf033c5963be74
Rafał Szypulewski <rafal.szypulewski@smtsoftware.com>
2015-05-07 12:13:42 +0200

    Add StringGenerator app

---
+++ StringGenerator/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Threading;
+
+namespace StringGenerator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private StringGen gen = null;
+        //generate new random string and save it to clipboard
+        private void generateButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (gen == null)
+            {
+                gen = new StringGen(CharStringToUse.Text);
+            }
+            else
+            {
+                gen.SetCharList(CharStringToUse.Text);
+            }
+            var finalString = gen.GenerateString(howLong.Text,
+                spaceEveryX.Text, spaceEveryY.Text, (bool)isInsertSpacesOn.IsChecked,
+                newLineEveryX.Text, newLineEveryY.Text, (bool)isInsertNewLineOn.IsChecked);
+
+            System.Windows.Clipboard.SetText(finalString);
+            if (finalString.Length > 100000)
+            {
+                finalString = "String is too long to display (whole string is in clipboard)";
+            }
+            output.Text = finalString;
+
+        }
+
+        //generate counter string
+        private void generateCounterString_Click(object sender, RoutedEventArgs e)
+        {
+            if (gen == null)
+            {
+                gen = new StringGen(CharStringToUse.Text);
+            }
+            else
+            {
+                gen.SetCharList(CharStringToUse.Text);
+            }
+
+            var finalString = gen.GenerateCounterString(counterStringSize.Text, counterStringSign.Text);
+
+            System.Windows.Clipboard.SetText(finalString);
+            if (finalString.Length > 100000)
+            {
+                finalString = "String is too long to display (whole string is in clipboard)";
+            }
+            output.Text = finalString;
+        }
+
+
+    }
+}
---
+++ StringGenerator/StringGen.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace StringGenerator
+{
+    class StringGen
+    {
+        private string charList;
+
+        public StringGen(string charString)
+        {
+            //charList = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            charList = charString;
+        }
+
+        public void SetCharList(string charString)
+        {
+            charList = charString;
+        }
+
+
+        public string GenerateString(string howLong,
+            string spaceEveryX, string spaceEveryY, bool isInsertSpacesOn,
+            string newLineEveryX, string newLineEveryY, bool isInsertNewLinesOn)
+        {
+            string finalString = "";
+
+            try
+            {
+                int size = int.Parse(howLong);
+
+                var stringChars = new char[size];
+                var random = new Random();
+                int countChars = 0;
+                int countCharsForNewLine = 0;
+                int everyXYinsertSpace = random.Next(int.Parse(spaceEveryX), (int.Parse(spaceEveryY) + 1));
+                int everyXYinsertNewLine = random.Next(int.Parse(newLineEveryX), (int.Parse(newLineEveryY) + 1));
+                bool isInsertSpaceChecked = isInsertSpacesOn;
+                bool isInsertNewLineChecked = isInsertNewLinesOn;
+                bool wasLastCharALetter = false;
+                for (int i = 0; i < stringChars.Length; i++)
+                {
+                    if (countChars >= everyXYinsertSpace && isInsertSpaceChecked && (i + 1 < stringChars.Length))
+                    {
+                        everyXYinsertSpace = random.Next(int.Parse(spaceEveryX), (int.Parse(spaceEveryY) + 1));
+                        stringChars[i] = ' ';
+                        countChars = 0;
+                        countCharsForNewLine++;
+                        wasLastCharALetter = false;
+                    }
+                    else
+                    {
+                        stringChars[i] = charList[random.Next(charList.Length)];
+                        countChars++;
+                        countCharsForNewLine++;
+                        wasLastCharALetter = true;
+                    }
+                    if (countCharsForNewLine >= everyXYinsertNewLine && isInsertNewLineChecked && (i + 2 < stringChars.Length) && wasLastCharALetter)
+                    {
+                        everyXYinsertNewLine = random.Next(int.Parse(newLineEveryX), (int.Parse(newLineEveryY) + 1));
+                        stringChars[i + 1] = Environment.NewLine[0];
+                        stringChars[i + 2] = Environment.NewLine[1];
+                        countChars = 0;
+                        countCharsForNewLine = 0;
+                        i += 2;
+                        wasLastCharALetter = false;
+                    }
+                }
+                finalString = new String(stringChars);
+            }
+            catch (Exception e)
+            {
+                finalString = e.Message + Environment.NewLine + Environment.NewLine + e.StackTrace;
+                MessageBox.Show(finalString);
+            }
+            return finalString;
+        }
+
+
+
+
+
+        public string GenerateCounterString(string howLong,
+            string counterStringChar)
+        {
+            string finalString = "";
+
+            try
+            {
+                int size = int.Parse(howLong);
+                int countSize = 0;
+                string numberString;
+                while (size > countSize)
+                {
+                    if ((size - countSize) == 1)
+                    {
+                        finalString = counterStringChar.First() + finalString;
+                        break;
+                    }
+                    numberString = (size - countSize).ToString();
+                    finalString = numberString + counterStringChar.First() + finalString;
+                    countSize = finalString.Length;
+                }
+            }
+            catch (Exception e)
+            {
+                finalString = e.Message + Environment.NewLine + Environment.NewLine + e.StackTrace;
+                MessageBox.Show(finalString);
+            }
+            return finalString;
+        }
+
+    }
+}
